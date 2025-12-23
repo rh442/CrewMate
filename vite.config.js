8c847212f9e4f3847d5da0d842447b0e90d46873
rh442 <116900219+rh442@users.noreply.github.com>
2025-12-23 16:13:31 -0500

    Change base path in Vite config to 'CrewMate'

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/crewmate/', // Replace with your repo name
+  base: '/CrewMate/', // Replace with your repo name
 })
